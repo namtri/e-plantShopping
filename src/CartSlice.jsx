500e7975f2edd81041eaf2cde756e047450c9faa
JD Martin <namtri@gmail.com>
2025-04-22 14:57:09 -0400

    Working on the cart functionality and making the components play nice

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -20,9 +20,9 @@ export const CartSlice = createSlice({
     },
     updateQuantity: (state, action) => {
         const { name, quantity } = action.payload;
-        const existingItem = state.items.find(item => item.name === name);
-        if (existingItem) {
-            existingItem.quantity = quantity;
+        const itemToUpdate = state.items.find(item => item.name === name);
+        if (itemToUpdate) {
+            itemToUpdate.quantity = quantity;
         }
     },
   },
